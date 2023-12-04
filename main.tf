3997b0392a2e78faf7385232a3f5fb92a441bfcb
SriniSrini21 <91723490+SriniSrini21@users.noreply.github.com>
2023-12-04 09:58:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.00.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-githubactions"
+  location = "West Europe"
+}
