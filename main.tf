d2cafc9ec8bd72e061dcdbde3a57180b89dd0c85
SriniSrini21 <91723490+SriniSrini21@users.noreply.github.com>
2023-12-04 14:47:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
   features {
 
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   =  secrets.AZURE_CLIENT_SECRET
-  tenant_id       = "10000000-0000-0000-0000-000000000000"
-  subscription_id = "20000000-0000-0000-0000-000000000000"
+  client_id       = "b3cd955f-8544-41d2-aee4-7e0cb6a5bd2d"
+  client_secret   =  "xuy8Q~Ktm3VVRtO~5aTB3Cu6Jp3KUI6O7Ftroazx"
+  tenant_id       = "5ce5fed3-0a25-4f4d-8697-e4b70519b7f2"
+  subscription_id = "79b014ea-6a1a-4930-9818-fddc78b6ce5c"
 
   }
 }
